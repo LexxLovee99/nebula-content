31b219d70afff6dcc5d6e17bed8dc091108bc4e4
Aulexis Rochell <aulexis.rochell@yahoo.com>
2023-03-07 20:50:52 -0500

    finish

--- 2-numbers/assignments/test .js
+++ 2-numbers/assignments/test .js
@@ -1,1 +1,26 @@
 //test 
+
+// Create a .js file that satisfies assignment below 
+// Copy the final assignment text into this assignment to submit
+// For each exercise you'll want to log the result to the console to check your work
+
+
+//1. Create a set of variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
+const name = 'tiger'
+const age = 20
+const height = "6' 10"
+const weight = 200
+const furcolor = "stripped black and orange "
+const diet= 'carnivore'
+console.log(name, age,height,weight,furcolor,diet);
+
+
+
+//2. Create a set of variables that define a vehicle. Must be at least 6 variables.
+const carname = 'Lexus'
+const year = 1989
+const model= 'Lexus GX'
+const price= 47400
+const gas= 'premium 91-octane'
+const color= 'red'
+console.log(carname,year,model,price,gas,color);
